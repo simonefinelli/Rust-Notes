57f58baf2de21500117239d69c5417c37c4a5a3b
Simone Finelli <simonefinelli.work@gmail.com>
2023-11-14 17:08:20 +0100

    Feat: update primitive types section

--- Beginner/01_DataTypes/_04_primitive_types/src/main.rs
+++ Beginner/01_DataTypes/_04_primitive_types/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     // Integers
     //  - Signed: i8, i16, i32, i64
     //	- Unsigned: u8, u16,u32,u64
+    let i1:i8 = 3;
+    let i2:u128 = 100 * 1_000_000;
     println!("Max value for an unsigned integer of 8bit: {}", u8::MAX);
 
     // Floats
@@ -24,8 +26,14 @@ fn main() {
     let c3 = '+';
     let c4 = '\u{288A}';
     let c5 = '\"';
-    println!(
-        "The value of c1 is {}, c2 is {}, c3 is {}, c4 is {} and c5 is {}",
-        c1, c2, c3, c4, c5
-    );
+    println!("The value of c1 is {c1}, c2 is {c2}, c3 is {c3}, c4 is {c4} and c5 is {c5}");
+
+    // Type Aliasing
+    type Age = u8;
+    let my_age: Age = 42;
+
+    // Type Conversion (as operator)
+    let float_var = 100.89;
+    let int_var = float_var as i64;
+    println!("The integer value is: {int_var}");
 }
